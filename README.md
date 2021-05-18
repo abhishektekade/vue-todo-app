12954955b056b713ff82d8464c825c590e7a1490
Abhishek Tekade <abhishektekade45@gmail.com>
2021-05-18 12:10:36 +0530

    This is first commit

--- README.md
+++ README.md
@@ -1,24 +1,43 @@
 # todo-app
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+# Todo App
+
+Tasks
+
+- [x] Basic Form, Input, Button
+- [x] Basic list Display in Page
+- [x] Get user Input Task
+- [x] Display it in console
+- [x] Add that Task to List
+- [x] Clear Previous Input
+- [x] Get checkboxes or a line So we can know Done Task and Not Done
+  - [x] Bool value True or false
+- [] Delete task Button
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,86 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+  <div>
+    <form action="" @submit.prevent="formSubmitted">
+      <input type="text" v-model="newTask" />
+      <div class="below-search"><button>Add</button></div>
+      <!-- <button @click="removeAll">Remove All</button> -->
+    </form>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        <h1 :class="{ done: todo.done }" @click="done(todo)">
+          {{ todo.title }}
+        </h1>
+        <button @click="removeTodo(index)">Remove</button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { ref } from "vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  setup() {
+    let newTask = ref("");
+    let todos = ref([]);
+    function formSubmitted() {
+      todos.value.push({
+        done: false,
+        title: newTask.value,
+      });
+      newTask.value = "";
+    }
+
+    function done(todo) {
+      todo.done = !todo.done;
+    }
+
+    function removeTodo(index) {
+      todos.value.splice(index, 1);
+    }
+    // function removeAll(index) {}
+
+    return {
+      title: "Todo App",
+      newTask,
+      formSubmitted,
+      todos,
+      done,
+      removeTodo,
+      // removeAll,
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+* body {
+  margin: 0;
+  padding: 0;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+li {
+  list-style: none;
+}
+input {
+  width: 80%;
+  height: 25px;
+  padding: 2px;
+  font-size: 20px;
+}
+.done {
+  text-decoration: line-through;
+}
+/* .below-search {
+  height: 25px;
+  width: 80%;
+} */
+button {
+  height: 35px;
+  width: 81%;
+  padding: 5px;
+  margin: 10px;
 }
 </style>
